e6c7b4311299d13b8552f655eedacb15e96633ab
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-07-15 22:33:18 -0300

    Criado EndPoint create() e findById() na Classe ProductController

--- product-catalog/src/main/java/com/digitalinnovation/experts/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/com/digitalinnovation/experts/productcatalog/controller/ProductController.java
@@ -2,10 +2,9 @@ package com.digitalinnovation.experts.productcatalog.controller;
 
 import com.digitalinnovation.experts.productcatalog.model.Product;
 import com.digitalinnovation.experts.productcatalog.repository.ProductRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/product")
@@ -20,5 +19,11 @@ public class ProductController {
     @PostMapping
     public Product create( @RequestBody Product product ){
         return productRepository.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product findById( @PathVariable Long id ){
+        return productRepository.findById(id)
+                .orElseThrow( () -> new ResponseStatusException( HttpStatus.NOT_FOUND, "Produto não encontrado" ));
     }
 }
